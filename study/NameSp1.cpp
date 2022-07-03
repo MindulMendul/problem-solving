d15446df29a947379933e110d5a9acab0e0d601e
mindul <pencake33@naver.com>
2022-07-03 23:24:57 +0900

    1-5 using namespace 까지

---
+++ study/NameSp1.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//namespace BestConImp1 {
+//	void SimpleFunc(void) {
+//		std::cout << "BestCom이 정의한 함수" << std::endl;
+//	}
+//}
+//
+//namespace ProgConImp1 {
+//	void SimpleFunc(void) {
+//		std::cout << "ProgCom이 정의한 함수" << std::endl;
+//	}
+//}
+//
+//int main(void) {
+//	BestConImp1::SimpleFunc();
+//	ProgConImp1::SimpleFunc();
+//	return 0;
+//}
