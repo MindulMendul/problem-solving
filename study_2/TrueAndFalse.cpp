e49c3985f81cd503c22328bbb575d069d6008471
mindul <pencake33@naver.com>
2022-07-04 23:28:47 +0900

    2-1 bool 변수까지 공부

---
+++ study_2/TrueAndFalse.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void) {
+//	int num = 10;
+//	int i = 0;
+//
+//	cout << "true: " << true << endl;
+//	cout << "false: " << false << endl;
+//
+//	while (true) {
+//		cout << i++ << ' ';
+//		if (i > num) break;
+//	}
+//	cout << endl;
+//
+//	cout << "stzeof 1: " << sizeof(1) << endl;
+//	cout << "stzeof 0: " << sizeof(0) << endl;
+//	cout << "stzeof true: " << sizeof(true) << endl;
+//	cout << "stzeof false: " << sizeof(false) << endl;
+//	return 0;
+//}
