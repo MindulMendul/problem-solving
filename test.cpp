fe74795a85efc462081dbdcc3bf26709c1dcfa48
Mindul <pencake33@naver.com>
2024-10-14 17:15:57 +0900

    vs code 설정 날림

--- test.cpp
+++ test.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
+#include <string>
 
 using namespace std;
-
-typedef long long int lli;
-
-static lli arr[100000];
-
-int solve(int res) {
-	
-}
 
 int main(){
     freopen("input.txt", "rt", stdin);
@@ -18,22 +9,50 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    lli N, M;
-		int i, j=0;
-    
-		cin>>N;
-		for(i=0; i<N; i++){
-			cin>>arr[i];
+	bool Set[20]={false, false, false, false, false,  false, false, false, false, false,  false, false, false, false, false,  false, false, false, false, false};
+	for(i=0; i<N; i++){
+		cin>>arr[i];
+	}
+
+	cin>>M;
+
+	for(i=0; i<M; i++){
+		string cmd; int j;
+		cin >> cmd;
+		switch(cmd){
+			case "add":
+				cin>>j;
+				Set[j-1]=true;
+				break;
+
+			case "remove":
+				cin>>j;
+				Set[j-1]=false;
+				break;
+
+			case "check":
+				cin>>j;
+				j=(Set[j-1])?1:0;
+				cout<<j<<endl;
+				break;
+
+			case "toggle":
+				cin>>j;
+				Set[j-1]=!Set[j-1];
+				break;
+
+			case "all": 
+				for(j=0 j<20; j++){
+					Set[j]=true;
+				}
+				break;
+			case "empty": 
+				for(j=0 j<20; j++){
+					Set[j]=true;
+				}
+				break;
 		}
-
-		sort(arr, arr + N);
-
-		cin>>M;
-
-		for(i=0; i<M; i++){
-			cin >> j;
-			cout << solve(j) << "\n";
-		}
+	}
 		
     return 0;
 }
